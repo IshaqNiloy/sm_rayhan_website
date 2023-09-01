6fbd5c27bad86fc04347d2eae3e9b23c784c8eda
IshaqNiloy <ishaqniloy1996@gmail.com>
2023-09-01 22:17:27 +0600

    embedded moments model added

--- moments/admin.py
+++ moments/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Moment
+from .models import Moment, EmbeddedMoment
 # Register your models here.
 
 
@@ -8,3 +8,11 @@ class MomentAdmin(admin.ModelAdmin):
     list_display = ['associated_to', 'image', 'alternate_text', 'header', 'description']
     list_filter = ['associated_to']
     search_fields = ['associated_to']
+
+
+@admin.register(EmbeddedMoment)
+class EmbeddedMomentAdmin(admin.ModelAdmin):
+    list_display = ['associated_to', 'image', 'alternate_text', 'header', 'description']
+    list_filter = ['associated_to']
+    search_fields = ['associated_to']
+
--- moments/models.py
+++ moments/models.py
@@ -17,3 +17,21 @@ class Moment(models.Model):
 
     def __str__(self):
         return self.description
+
+
+class EmbeddedMoment(models.Model):
+    moment = models.ForeignKey(Moment, on_delete=models.CASCADE)
+    id = models.BigAutoField(primary_key=True, editable=False)
+    associated_to = models.CharField(max_length=250)
+    image = models.FileField()
+    alternate_text = models.CharField(max_length=250)
+    header = models.CharField(max_length=250)
+    description = models.CharField(max_length=1000)
+
+    class Meta:
+        verbose_name = 'embedded moment'
+        verbose_name_plural = 'embedded moments'
+
+    def __str__(self):
+        return self.description
+
